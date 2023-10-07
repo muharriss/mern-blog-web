7250252e69f7b3057fb2a3bf874d8c091f9e67a3
muharriss <muhharris@gmail.com>
2023-10-07 16:58:30 +0700

    updated

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <div>
       <div className='Parallax Intro'>
-        <p className='blog-title animate__animated animate__fadeIn animate__slow'>welcome</p>
+        <p className='blog-title animate__animated animate__fadeIn animate__slow font-intro'><span className='fast-flicker'>m</span>uhar<span className='flicker'>r</span>is</p>
         {/* <div className='intro-line-wrapper'>
           <p className='intro-line  animate__animated animate__bounce animate__slow'>|</p>
         </div> */}
